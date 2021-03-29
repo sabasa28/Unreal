9478e07bfdb06faa784de3ed51f3c3ac754bebfe
Iñaki Diez <inakidiezgalarza@gmail.com>
2021-03-29 19:53:31 -0300

    save

---
+++ Source/S02C02_GFBlueprints/GM_test2.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_test2.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class S02C02_GFBLUEPRINTS_API AGM_test2 : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
